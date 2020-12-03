048cc1efc52cdc870161629c2ada49c9c1796afa
Tromp <tromphakvoort@outlook.com>
2020-12-03 17:30:08 +0100

    Feat: loading ffmpeg on page load

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+
+const ffmpeg = createFFmpeg({ log: true });
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+  const [ready, setReady] = useState(false);
+
+  const load = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  };
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  return ready ? <p>Ready</p> : <p>loading...</p>;
 }
 
 export default App;
